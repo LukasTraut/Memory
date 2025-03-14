9832d6684c5d24e53c5ed281af44aa4a5a6e2747
trl <lukas.traut@medidata.ch>
2025-03-14 11:36:31 +0100

    chore: rebuild gh-pages based on bkp

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './styles.css'
+
+function App() {
+
+    return (
+        <>
+            <header>
+                <title>Memory: The Game</title>
+                <h1>Memory: The Game</h1>
+            </header>
+            <div className="App">
+                <p>Body, hier ist das Memory</p>
+            </div>
+            <footer>
+                <p>Memory: The Game created by Lukas Traut</p>
+            </footer>
+
+        </>
+    )
+}
+
+export default App
