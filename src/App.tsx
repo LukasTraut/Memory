6111f2a4b81ee937b346efe4e7f4e4d7d7718898
trl <lukas.traut@medidata.ch>
2025-03-14 11:36:31 +0100

    chore: rebuild gh-pages based on bkp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import "./styles.css";
-import MemoryTable from "./components/MemoryTable";
+
+import './styles.css'
 
 function App() {
-  return (
-    <>
-      <head>
-        <title>Memory: The Game</title>
-      </head>
-      <header>
-        <h1>Memory: The Game</h1>
-      </header>
-      <div className="App">
-        <MemoryTable initialColumns={6} initialRows={4} />
-      </div>
-      <footer>
-        <p>Memory: The Game created by Lukas Traut</p>
-      </footer>
-    </>
-  );
+
+    return (
+        <>
+            <header>
+                <title>Memory: The Game</title>
+                <h1>Memory: The Game</h1>
+            </header>
+            <div className="App">
+                <p>Body, hier ist das Memory</p>
+            </div>
+            <footer>
+                <p>Memory: The Game created by Lukas Traut</p>
+            </footer>
+
+        </>
+    )
 }
 
-export default App;
+export default App
