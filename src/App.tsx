0f79de90105e68b0b72b074ce710b0f028bab40a
trl <lukas.traut@medidata.ch>
2025-03-26 08:42:32 +0100

    Head added

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,19 @@ function App() {
 
     return (
         <>
-            <div className="App">
+            <head>
+                <title>Memory: The Game</title>
+            </head>
+            <header>
                 <h1>Memory: The Game</h1>
+            </header>
+            <div className="App">
+                <p>Body, hier ist das Memory</p>
             </div>
+            <footer>
+                <p>Memory: The Game created by Lukas Traut</p>
+            </footer>
+
         </>
     )
 }
