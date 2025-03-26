2b0cfa4fb149d35fa707a14497729e55ad63020b
trl <lukas.traut@medidata.ch>
2025-03-26 08:55:49 +0100

    Prettier über Files laufen lassen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-
-import './styles.css'
+import "./styles.css";
 
 function App() {
-
-    return (
-        <>
-            <head>
-                <title>Memory: The Game</title>
-            </head>
-            <header>
-                <h1>Memory: The Game</h1>
-            </header>
-            <div className="App">
-                <p>Body, hier ist das Memory</p>
-            </div>
-            <footer>
-                <p>Memory: The Game created by Lukas Traut</p>
-            </footer>
-
-        </>
-    )
+  return (
+    <>
+      <head>
+        <title>Memory: The Game</title>
+      </head>
+      <header>
+        <h1>Memory: The Game</h1>
+      </header>
+      <div className="App">
+        <p>Body, hier ist das Memory</p>
+      </div>
+      <footer>
+        <p>Memory: The Game created by Lukas Traut</p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
