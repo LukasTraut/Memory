f87e8199166d9626986cc0089d52148a6fd8462c
trl <lukas.traut@medidata.ch>
2025-04-23 09:17:18 +0200

    Citys/Images for the first 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-
-import './styles.css'
-import MemoryTable from './components/MemoryTable';
+import "./styles.css";
+import MemoryTable from "./components/MemoryTable";
 
 function App() {
-
-    return (
-        <>
-            <head>
-                <title>Memory: The Game</title>
-            </head>
-            <header>
-                <h1>Memory: The Game</h1>
-            </header>
-            <div className="App">
-                <MemoryTable initialColumns={6} initialRows={4} />
-            </div>
-            <footer>
-                <p>Memory: The Game created by Lukas Traut</p>
-            </footer>
-
-        </>
-    )
+  return (
+    <>
+      <head>
+        <title>Memory: The Game</title>
+      </head>
+      <header>
+        <h1>Memory: The Game</h1>
+      </header>
+      <div className="App">
+        <MemoryTable initialColumns={6} initialRows={4} />
+      </div>
+      <footer>
+        <p>Memory: The Game created by Lukas Traut</p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
